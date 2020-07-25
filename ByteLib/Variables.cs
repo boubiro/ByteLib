760ebdcc6320e7b3ea6c292d1bc7edeeb4014186
Benjamin <60155141+Biitez@users.noreply.github.com>
2020-07-25 16:11:33 -0400

    Update Variables.cs

--- ByteLib/Variables.cs
+++ ByteLib/Variables.cs
@@ -1,17 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using xNet;
 
-namespace CheckerLib
+namespace ByteLib
 {
     public class Variables
     {
         public static int Progress { get; set; }
+        public static List<string> ComboList;
+        public List<string> ProxyList { get; set; }
+        public ProxyType proxyType { get; set; }
+        public static int Threads { get; set; }
         public static int ComboLenght { get; set; }
         public static int Hits { get; set; }
         public static int Fails { get; set; }
         public static int Retries { get; set; }
+        public static int Index { get; set; }
     }
 }
